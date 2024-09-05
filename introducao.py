25ca243450fa86be64f909fd9e23512c145f777c
luispaulo213 <127565042+luispaulo213@users.noreply.github.com>
2024-09-05 20:30:40 -0300

    Update introducao.py

--- introducao.py
+++ introducao.py
@@ -1,8 +1,7 @@
 import random
-import criaçãodepersonagem
-def introducao():
+def introducao(forcausuario,destrezausuario,constituicaousuario,inteligenciausuario,sabedoriausuario,carismausuario,nomeusuario,classeusuario,racausuario):
     print('lorem')
-    print('bom agora que você estpá aqui nessa simulação vamostreinar uma coisa básica movimentação')
+    print('bom agora que você está aqui nessa simulação vamos treinar uma coisa básica movimentação')
     print('WIP')
     listnames = ['Alaric', 'Beatrix', 'Cedric', 'Dante', 'Eleanor', 'Faramir', 'Giselle', 'Harold', 'Isolde', 'Jocelyn', 'Leofric', 'Matilda', 'Niall', 'Odette', 'Percival', 'Quillian', 'Rhiannon', 'Sigmund', 'Thaddeus', 'Ulf', 'Violetta', 'Wulfric', 'Yseult', 'Agravain', 'Brynhild', 'Catriona', 'Dorian', 'Eadric', 'Fenella', 'Godfrey', 'Hildegarde', 'Ingmar', 'Jareth', 'Kenelm', 'Lancelot', 'Morgana', 'Osric', 'Rowena', 'Theobald', 'Wilfred']
     nomeinimigo = random.choice(listnames)
@@ -13,13 +12,16 @@ def introducao():
     velocidadeinimigo = 16
     carismainimigo = 16
     percepcaoinimigo = 16
+    speedinimigo = 16
+    
     #atazanaplayer2.0 clocar em vários prints uma coisa que podia ser resumida e normalmente é "primeiro vc vai na agressão"
-    print(f'agora vamos começar a batalha seu inimigo é {nomeinimigo}')
-    print('vamos lá aqui nessa interface inicial de uma batalha você tem 3 opções tentar fugir que não é o objetivo de hoje, atacar e itens agor aeu quero que você entre em itens')
+    print(f'agora vamos começar a batalha seu inimigo é {nomeinimigo}.')
+    print('Vamos lá, aqui nesta interface inicial de batalha você tem 3 opções tentar fugir, batalhar e itens. Agora vamos ver as opções de itens.')
 #jogadorseleciona itens
-    print('aqui você pode trocar a arma escudo, armadura agora vamos voltar ao menu principal e ir na batalha')
+    print('Aqui tudo é dividio por seções e dentro delas tem mais seções e assim vai, mas eu vou te apresentar as seções em ordem.')
+    print('A primeira seção é armas')
 #jogador vai par o menu e selciona atacar e a opção ataque 
     print('bom como pode ver você tem 4 ataques que variam de acordo com as suas escolhas de classe e raça agora vamos ver as opções de defesa')
 #jogador volta e seleciona defesa
     print('bom agora em defesa você tem duas opções o escudo que gasta x de stamina ou a esquiva que gasta y de stamina')
-print(introducao())
+    return 
